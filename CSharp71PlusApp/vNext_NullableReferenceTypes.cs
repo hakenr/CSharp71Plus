f5457a24d4525daf6b55b6a2ff5c7fbcde5f170f
Robert Haken <haken@havit.cz>
2018-01-30 11:17:26 +0100

    C# vNext - Nullable Reference Types

---
+++ CSharp71PlusApp/vNext_NullableReferenceTypes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp71PlusApp
+{
+	public class NullableReferenceTypes
+	{
+		//public void Demo()
+		//{
+		//	string x = null;	// compiler warning
+		//	Console.Write(x);
+
+		//	string? x = null;	// compiler OK
+		//	Console.Write(x);	// compiler warning
+		//}
+
+		//public void DoSomething(string! myNonNullableParameter)  // auto-generates a runtime null check
+		//{
+
+		//}
+	}
+}

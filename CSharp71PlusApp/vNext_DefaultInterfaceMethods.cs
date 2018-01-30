f4cb93554ffc5efa3e83fae8b71eed98d89c1be9
Robert Haken <haken@havit.cz>
2018-01-30 11:48:23 +0100

    C# vNext - Default interface methods

---
+++ CSharp71PlusApp/vNext_DefaultInterfaceMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp71PlusApp
+{
+	//interface IEnumerable<T>
+	//{
+	//	int Count()
+	//	{
+	//		int count = 0;
+	//		foreach (var x in this)
+	//			count++;
+	//		return count;
+	//	}
+	//}
+	//interface IList<T> : IEnumerable<T>
+	//{
+	//	int Count { get; }
+	//	override int IEnumerable<T>.Count() => this.Count;
+	//}
+}
